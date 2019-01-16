9f3ab3f0b66d0513ce382dc03463c11584e4aad1
Eugene Stativka <stativkaea@gmail.com>
2019-01-16 23:49:33 +0100

    Add canvas element

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { css } from "emotion";
+import { CanvasAnimation } from "./CanvasAnimation";
 import { CellStates } from "./CellStates";
 
 window.document.body.classList.add(css`
@@ -6,3 +7,13 @@ window.document.body.classList.add(css`
 `);
 
 window.document.body.innerHTML = String(CellStates.Alive);
+
+// @ts-ignore
+const canvasAnimation = new CanvasAnimation({
+  render: (ctx: CanvasRenderingContext2D) => () => {
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+  },
+  target: window.document.body,
+});
