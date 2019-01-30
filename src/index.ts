5106371ce2e2972d9cb80505b71479c8aa33593b
Eugene Stativka <stativkaea@gmail.com>
2019-01-30 17:26:03 +0100

    Use scan operator for a state

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { css } from "emotion";
 import { animationFrameScheduler, interval } from "rxjs";
-import { map, pairwise, startWith, tap } from "rxjs/operators";
+import { scan } from "rxjs/operators";
 import { render } from "./canvas";
 import { CellStates, Life } from "./types";
 
@@ -19,40 +19,41 @@ const life: Life = Array.from({ length: width }, () =>
 
 render({
   life$: interval(1000, animationFrameScheduler).pipe(
-    startWith(life),
-    pairwise(),
-    map(([prevLife]) =>
-      !Array.isArray(prevLife)
-        ? life
-        : (prevLife as Life).map((line, x) => {
-            return line.map((item, y) => {
-              const liveCellsAroundCount = [
-                (prevLife[x - 1] || [])[y - 1],
-                (prevLife[x] || [])[y - 1],
-                (prevLife[x + 1] || [])[y - 1],
-                (prevLife[x - 1] || [])[y],
-                (prevLife[x + 1] || [])[y],
-                (prevLife[x - 1] || [])[y + 1],
-                (prevLife[x] || [])[y + 1],
-                (prevLife[x + 1] || [])[y + 1],
-              ].reduce((acc, cell) => acc + (cell || 0), 0);
+    scan<number, Life>(
+      (prevLife, _, i) =>
+        i === 0
+          ? prevLife
+          : prevLife.map((line, x) =>
+              line.map((item, y) => {
+                const liveCellsAroundCount = [
+                  (prevLife[x - 1] || [])[y - 1],
+                  (prevLife[x] || [])[y - 1],
+                  (prevLife[x + 1] || [])[y - 1],
+                  (prevLife[x - 1] || [])[y],
+                  (prevLife[x + 1] || [])[y],
+                  (prevLife[x - 1] || [])[y + 1],
+                  (prevLife[x] || [])[y + 1],
+                  (prevLife[x + 1] || [])[y + 1],
+                ].reduce((acc, cell) => acc + (cell || 0), 0);
 
-              if (item === CellStates.Dead) {
-                return liveCellsAroundCount === 3
-                  ? CellStates.Alive
-                  : CellStates.Dead;
-              }
+                switch (true) {
+                  case item === CellStates.Dead && liveCellsAroundCount === 3:
+                    return CellStates.Alive;
 
-              if (liveCellsAroundCount === 2 || liveCellsAroundCount === 3) {
-                return CellStates.Alive;
-              }
+                  case item === CellStates.Dead && liveCellsAroundCount !== 3:
+                    return CellStates.Dead;
 
-              return CellStates.Dead;
-            });
-          }),
+                  case liveCellsAroundCount === 2:
+                  case liveCellsAroundCount === 3:
+                    return CellStates.Alive;
+
+                  default:
+                    return CellStates.Dead;
+                }
+              }),
+            ),
+      life,
     ),
-    // tslint:disable-next-line
-    tap(v => console.log(v)),
   ),
   target: window.document.getElementById("root") || window.document.body,
 });
--- src/types/CellStates.ts
+++ src/types/CellStates.ts
@@ -1,4 +1,4 @@
 export const enum CellStates {
-  Alive,
   Dead,
+  Alive,
 }
